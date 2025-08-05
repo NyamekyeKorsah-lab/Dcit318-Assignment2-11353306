d5c653f37acd39f0f2cdf5ecf1849f8618274c0a
Nyamekye Korsah <nkorsah001@st.ug.edu.gh>
2025-08-05 09:15:49 -0700

    Initial commit for DCIT 318 Assignment 1

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+    }
+}
